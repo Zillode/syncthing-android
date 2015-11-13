9f3bf83cfc860d862d67fd5ca56d791b37a063ff
Lode Hoste <zillode@zillode.be>
2015-11-13 23:38:17 +0100

    Use CGO to build Syncthing with support for DNS

---
+++ make-all.bash
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+# Build the syncthing library
+
+./make-go.bash arm
+./make-syncthing.bash arm
+
+./make-go.bash 386
+./make-syncthing.bash 386
+
+# Will need to bump target API to support this
+#./make-go.bash amd64
+#./make-syncthing.bash amd64
+
