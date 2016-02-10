a272302b26557c08b1842779888c6f343896e23f
Lode Hoste <zillode@zillode.be>
2016-02-10 19:07:38 +0100

    Build without cgo

--- make-all.bash
+++ make-all.bash
@@ -2,12 +2,12 @@
 
 # Build the syncthing library
 
-./make-go.bash arm
-./make-syncthing.bash arm
+./make-go-nocgo.bash arm
+./make-syncthing-nocgo.bash arm
 
-./make-go.bash 386
-./make-syncthing.bash 386
+./make-go-nocgo.bash 386
+./make-syncthing-nocgo.bash 386
 
-./make-go.bash amd64
-./make-syncthing.bash amd64
+./make-go-nocgo.bash amd64
+./make-syncthing-nocgo.bash amd64
 
